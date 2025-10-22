076e71de1e79689e256cb16d5d3a0660ba11b2eb
mohammed amaan <amaanvisiontech@gmail.com>
2025-10-22 14:34:40 +0530

    connect with mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const mongoose = require("mongoose");
 
 const PORT = 3000;
 
@@ -7,34 +8,46 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('🍔 Welcome to Node Food Truck! Visit /menu to see what’s cooking.');
+mongoose
+  .connect(
+    "mongodb+srv://ma:rzCsIZNE9ucIgJ92@cluster0.blyqlak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.log("MongoDB not connection erroe: ", err));
+
+const dishSchema = new mongoose.Schema({
+  item: String,
+  price: Number,
 });
 
-app.get("/menu", (req, res) => {
-  const menu = JSON.parse(fs.readFileSync('menu.json'));
-  res.json(menu);
+const Dish = mongoose.model("Dish", dishSchema);
+
+app.get("/", (req, res) => {
+  res.send("🍔 Welcome to Node Food Truck! Visit /menu to see what’s cooking.");
 });
 
-app.post("/menu", (req, res) => {
-    const menu = JSON.parse(fs.readFileSync('menu.json'));
-    const newItem = req.body;
-
-    newItem.id = menu.length ? menu[menu.length - 1].id + 1 : 1;
-    menu.push(newItem);
-
-    fs.writeFileSync('menu.json', JSON.stringify(menu,null,2));
-    res.status(201).json(newItem);
-
+app.get("/menu", async (req, res) => {
+  const dishes = await Dish.find();
+  res.json(dishes);
 });
 
-app.delete("/menu/:id", (req,res) => {
-    const menu = JSON.parse(fs.readFileSync('menu.json'));
-    const newMenu = menu.filter(item => item.id != req.params.id);
+app.post("/menu", async (req, res) => {
+  const newDish = new Dish(req.body);
+  const savedDish = await newDish.save();
+  res.status(201).json(savedDish);
+});
 
-    fs.writeFileSync('menu.json',JSON.stringify(newMenu,null,2));
-    res.json({message: `Item ${req.params.id} removed`});
-}); 
+app.put("/menu/:id", async (req, res) => {
+  const updatedDish = await Dish.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, // returns updated dish
+  });
+  res.json(updatedDish);
+});
+
+app.delete("/menu/:id", async (req, res) => {
+  await Dish.findByIdAndDelete(req.params.id);
+  res.json({ message: `Item ${req.params.id} removed.` });
+});
 
 app.listen(PORT, () => {
   console.log(`Food truck running on http://localhost:${PORT}`);
