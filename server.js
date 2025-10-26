bbf69ae661bff2fc833d8a863bc658b9679c440b
mohammed amaan <amaanvisiontech@gmail.com>
2025-10-26 19:16:44 +0530

    set cors policy

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
 
-const dishRoutes = require('./routes/dishRoutes');
-const userRoutes = require('./routes/userRoutes');
+const dishRoutes = require("./routes/dishRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(
+  express.json({
+    origin: ["https://food-truck-wtwh.vercel.app/"], // your actual frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 connectDB();
 
-app.get('/', (req, res) => res.send('🍔 Welcome to Food Truck API!'));
+app.get("/", (req, res) => res.send("🍔 Welcome to Food Truck API!"));
 
-app.use('/menu', dishRoutes);
+app.use("/menu", dishRoutes);
 
-app.use('/user', userRoutes);
+app.use("/user", userRoutes);
 
 app.listen(PORT, () =>
   console.log(`Food truck running on http://localhost:${PORT}`)
